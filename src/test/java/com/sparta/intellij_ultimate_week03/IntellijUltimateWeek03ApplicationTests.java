c152e25b3fb1c6a77ba5a74290bee2ae3c8bc17a
Jdoubleeyun <anjeeyun0767@gmail.com>
2021-11-15 23:10:22 +0900

    Initial commit

---
+++ src/test/java/com/sparta/intellij_ultimate_week03/IntellijUltimateWeek03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.intellij_ultimate_week03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijUltimateWeek03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
