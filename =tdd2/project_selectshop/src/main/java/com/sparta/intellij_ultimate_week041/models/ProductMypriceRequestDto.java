e012124e3f6e941c9b1e11a567573158211e4d8e
jdoubleeyun <82137367+Jdoubleeyun@users.noreply.github.com>
2021-11-19 20:18:34 +0900

    Delete =tdd2/project_selectshop directory

--- =tdd2/project_selectshop/src/main/java/com/sparta/intellij_ultimate_week041/models/ProductMypriceRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.intellij_ultimate_week041.models;
-
-import lombok.Getter;
-
-@Getter
-public class ProductMypriceRequestDto {
-    private int myprice;
-}
--- =tdd2/project_selectshop/src/main/java/com/sparta/intellij_ultimate_week041/models/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.intellij_ultimate_week041.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
